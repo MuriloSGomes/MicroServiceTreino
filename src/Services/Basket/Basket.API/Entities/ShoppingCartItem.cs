31754162af0507abd21ed74ee92ac5b247fbf4c7
Murilo Gomes <seno.murilo@hotmail.com>
2021-10-22 17:43:07 -0300

    Inicia implementação basket

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCartItem
+    {
+        public int Quantity { get; set; }
+        public int Color { get; set; }
+        public int Price { get; set; }
+        public int ProductId { get; set; }
+        public int ProductName { get; set; }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,26 @@
+﻿using Basket.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        public Task DeleteBasket(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ShoppingCart> GetBasket(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
